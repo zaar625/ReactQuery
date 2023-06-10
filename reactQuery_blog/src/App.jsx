fb3bbce3d0e29b574ff758d46eb6a341b9ce9cb2
이상윤 <zaar625@gmail.com>
2023-06-10 15:49:34 +0900

    add : react-query_blog initial setting

---
+++ reactQuery_blog/src/App.jsx
@@ -0,0 +1,22 @@
+
+import './App.css';
+import {
+  QueryClientProvider,
+  QueryClient
+} from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+
+function App() {
+  // 클라이언트가 가지고 있는 캐시와 모든 기본 옵션을 자녀 컴포넌트도 사용할 수 있게 합니다. 
+  const queryClient = new QueryClient()
+
+  return (
+    <QueryClientProvider client={queryClient}>
+    {/* The rest of your application */}
+    <div>Hello</div>
+    <ReactQueryDevtools initialIsOpen={false} />
+  </QueryClientProvider>
+  )
+}
+
+export default App
