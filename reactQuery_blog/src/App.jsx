41f7f189801974c79de14ef9c9fa2f121af446fd
이상윤 <zaar625@gmail.com>
2023-06-12 10:09:57 +0900

    add: react_query_blog post.jsx added

--- reactQuery_blog/src/App.jsx
+++ reactQuery_blog/src/App.jsx
@@ -5,6 +5,7 @@ import {
   QueryClient
 } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import Posts from './Posts'
 
 function App() {
   // 클라이언트가 가지고 있는 캐시와 모든 기본 옵션을 자녀 컴포넌트도 사용할 수 있게 합니다. 
@@ -13,7 +14,10 @@ function App() {
   return (
     <QueryClientProvider client={queryClient}>
     {/* The rest of your application */}
-    <div>Hello</div>
+    <div className='App'>
+      <h1>React-Query Blog</h1>
+      <Posts/>
+    </div>
     <ReactQueryDevtools initialIsOpen={false} />
   </QueryClientProvider>
   )
--- reactQuery_blog/src/Posts.jsx
+++ reactQuery_blog/src/Posts.jsx
@@ -1,8 +1,67 @@
+import { useEffect, useState } from "react";
+import {useQuery, useQueryClient} from '@tanstack/react-query';
 
+import PostDetail from "./PostDetail";
+
+const maxPostPage = 10;
+
+
+async function fetchPosts(pageNum) {
+  const response = await fetch(`https://jsonplaceholder.typicode.com/posts?_limit=10&_page=${pageNum}`);
+
+  return response.json();
+}
 
 function Posts() {
+
+  const [currentPage, setCurrentPage] = useState(1);
+  const [selectedPost, setSelectedPost] = useState(null);
+  
+  const queryClient = useQueryClient();
+
+  useEffect(()=>{
+    if(currentPage < maxPostPage){
+      const nextPage = currentPage + 1;
+      queryClient.prefetchQuery(['posts', nextPage], ()=> fetchPosts(nextPage))
+    }
+  },[currentPage, queryClient]);
+
+  const {data , isError, error, isLoading} = useQuery(['post', currentPage],()=>fetchPosts(currentPage), {
+    staleTime:2000,
+    keepPreviousData:true
+  })
+
+  console.log(data);
+
+  if(isLoading) return <h3>Loading... ... </h3>;
+  
+  if(isError) 
+    return (
+      <>
+        <h3>Oops, something went wrong</h3>
+        <p>{error.toString()}</p>
+      </>
+  )
+
   return (
-    <div>Posts</div>
+    <>
+      <ul>
+        {
+          data.map((post) => (
+            <li key={post.id} className="post-title" onClick={()=>setSelectedPost(post)}>{post.title}</li>
+          ))
+        }
+      </ul>
+      <div className="pages">
+        <button disabled={currentPage <= 1} onClick={()=>setCurrentPage((prev) => prev -1)}>Previous page</button>
+        <span>Page is {currentPage}</span>
+        <button disabled={currentPage >= maxPostPage} onClick={()=> setCurrentPage((prev) => prev + 1)}>
+          Next Page
+        </button>
+      </div>
+      <hr/>
+      {selectedPost && <PostDetail post={selectedPost}/>}
+    </>
   )
 }
 
